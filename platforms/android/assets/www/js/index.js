5911140044da8247affcd0cdae03dcbb4dd5bda0
sgastanadui <santiagogerman@hotmail.com>
2015-05-27 10:56:10 -0500

    update

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -21,6 +21,7 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -32,18 +33,134 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
+        alert('onDeviceReady');
+
+        try {
+            var pushNotification = window.plugins.pushNotification;
+
+            $("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
+
+            if (device.platform == 'android' || device.platform == 'Android' || device.platform == 'amazon-fireos') {
+                //alert('Android');
+                pushNotification.register(app.successHandler, app.errorHandler, {
+                    "senderID": "AIzaSyBcJzo8GpAqy76BGtYfEtWkTe6WOLyXe_k",
+                    "ecb": "app.onNotification"
+                }); // required!
+                alert('register');
+            } else {
+                //alert('IOS');
+                pushNotification.register(app.tokenHandler, app.errorHandler, {
+                    "badge": "true",
+                    "sound": "true",
+                    "alert": "true",
+                    "ecb": "app.onNotificationAPN"
+                }); // required!
+            }
+
+        } catch (err) {
+            txt = "There was an error on this page.\n\n";
+            txt += "Error description: " + err.message + "\n\n";
+            alert(txt);
+        }
+
         app.receivedEvent('deviceready');
     },
+
+    tokenHandler : function (result) {
+        alert('device token = ' + result);
+        // Your iOS push server needs to know the token before it can push to this device
+        // here is where you might want to send it the token for later use.
+    },
+
+    successHandler: function (result) {
+        alert('device token = ' + result);
+    },
+
+    errorHandler: function (error) {
+        alert(error);
+    },
+
+    // handle APNS notifications for iOS
+    onNotificationAPN: function (e) {
+        alert(e.event);
+
+        if (e.alert) {
+            // showing an alert also requires the org.apache.cordova.dialogs plugin
+            navigator.notification.alert(e.alert);
+        }
+        if (e.sound) {
+            // playing a sound also requires the org.apache.cordova.media plugin
+            var snd = new Media(e.sound);
+            snd.play();
+        }
+        if (e.badge) {
+            pushNotification.setApplicationIconBadgeNumber(app.successHandler, app.errorHandler, e.badge);
+        }
+    },
+
+    // handle GCM notifications for Android
+    onNotification: function (e) {
+        alert(e.event);
+
+        switch (e.event) {
+            case 'registered':
+                if (e.regid.length > 0) {
+                    $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+                    // Your GCM push server needs to know the regID before it can push to this device
+                    // here is where you might want to send it the regID for later use.
+                    console.log("regID = " + e.regid);
+                }
+            break;
+            case 'message':
+                // if this flag is set, this notification happened while we were in the foreground.
+                // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+                if (e.foreground) {
+                    $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+                    // on Android soundname is outside the payload.
+                    // On Amazon FireOS all custom attributes are contained within payload
+                    var soundfile = e.soundname || e.payload.sound;
+                    // if the notification contains a soundname, play it.
+                    var my_media = new Media("/android_asset/www/" + soundfile);
+                    my_media.play();
+                }
+                else {  // otherwise we were launched because the user touched a notification in the notification tray.
+                    if (e.coldstart) {
+                        $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                    }
+                    else {
+                        $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+                    }
+                }
+
+                $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+                //Only works for GCM
+                $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+                //Only works on Amazon Fire OS
+                $status.append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
+
+                break;
+            case 'error':
+                $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+                break;
+            default:
+                $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+            break;
+        }
+    },
+
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function (id) {
+        //alert('receivedEvent');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        //var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
 
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+        $("#app-status-ul").append('<li>Received Event: ' + id + '</li>');
         console.log('Received Event: ' + id);
     }
 };
