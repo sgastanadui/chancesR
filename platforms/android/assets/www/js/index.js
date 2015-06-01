a1f0fa34e306f3e08fbda744b9cd66be9ac7a083
sgastanadui <santiagogerman@hotmail.com>
2015-06-01 01:37:39 -0500

    update

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -192,26 +192,24 @@ var app = {
                 // if this flag is set, this notification happened while we were in the foreground.
                 // you might want to play a sound to get the user's attention, throw up a dialog, etc.
                 if (e.foreground) {
-                    //$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-
                     // on Android soundname is outside the payload.
                     // On Amazon FireOS all custom attributes are contained within payload
                     var soundfile = e.soundname || e.payload.sound;
                     // if the notification contains a soundname, play it.
-                    //$("#app-status-ul").append('<li>Soundfile:' + soundfile + "</li>");
-                    var my_media = new Media("/android_asset/www/" + soundfile);
+                    var my_media = new Media("/android_asset/www/" + e.soundname);
                     my_media.play();
                 }
                 else {  // otherwise we were launched because the user touched a notification in the notification tray.
                     if (e.coldstart) {
                         //$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+                        navigator.notification.alert(e.payload.message, function () { }, "BCP Notification");
                     }
                     else {
                         //$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+                        navigator.notification.alert(e.payload.message, function () { }, "BCP Notification");
                     }
                 }
                 //alert(e.payload.message);
-                navigator.notification.alert(e.payload.message, function () { }, "BCP Notification");
 
                 //$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
                 //Only works for GCM
