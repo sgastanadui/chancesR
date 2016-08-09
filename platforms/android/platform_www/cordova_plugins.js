c17727bdcdb50baa651328eabfdecc5750cfe966
sgastanadui <santiagogerman@hotmail.com>
2016-08-09 12:23:19 -0500

    a

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,22 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "pluginId": "com.phonegap.plugins.PushPlugin",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-actionsheet.ActionSheet",
+        "file": "plugins/cordova-plugin-actionsheet/www/ActionSheet.js",
+        "pluginId": "cordova-plugin-actionsheet",
+        "clobbers": [
+            "window.plugins.actionsheet"
+        ]
+    },
+    {
         "id": "cordova-plugin-dialogs.notification",
         "file": "plugins/cordova-plugin-dialogs/www/notification.js",
         "pluginId": "cordova-plugin-dialogs",
@@ -235,31 +251,56 @@ module.exports = [
         "clobbers": [
             "Connection"
         ]
+    },
+    {
+        "id": "org.flybuy.cordova.background-location-services.BackgroundLocationServices",
+        "file": "plugins/org.flybuy.cordova.background-location-services/www/BackgroundLocationServices.js",
+        "pluginId": "org.flybuy.cordova.background-location-services",
+        "clobbers": [
+            "plugins.backgroundLocationServices"
+        ]
+    },
+    {
+        "id": "org.frd49.cordova.exitapp.CDVExitApp",
+        "file": "plugins/org.frd49.cordova.exitapp/www/exitApp.js",
+        "pluginId": "org.frd49.cordova.exitapp",
+        "clobbers": [
+            "navigator.app"
+        ]
+    },
+    {
+        "id": "uk.co.workingedge.phonegap.plugin.launchnavigator.Common",
+        "file": "plugins/uk.co.workingedge.phonegap.plugin.launchnavigator/www/common.js",
+        "pluginId": "uk.co.workingedge.phonegap.plugin.launchnavigator",
+        "clobbers": [
+            "launchnavigator"
+        ]
+    },
+    {
+        "id": "uk.co.workingedge.phonegap.plugin.launchnavigator.LaunchNavigator",
+        "file": "plugins/uk.co.workingedge.phonegap.plugin.launchnavigator/www/android/launchnavigator.js",
+        "pluginId": "uk.co.workingedge.phonegap.plugin.launchnavigator",
+        "merges": [
+            "launchnavigator"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-<<<<<<< HEAD
     "com.phonegap.plugins.PushPlugin": "2.5.0",
     "cordova-plugin-actionsheet": "2.2.2",
     "cordova-plugin-compat": "1.0.0",
-=======
     "cordova-plugin-console": "1.0.3",
->>>>>>> parent of 1b99a16... update changes
     "cordova-plugin-dialogs": "1.2.1",
-    "cordova-plugin-compat": "1.0.0",
     "cordova-plugin-file": "4.2.0",
     "cordova-plugin-geolocation": "2.2.0",
     "cordova-plugin-media": "2.3.0",
     "cordova-plugin-network-information": "1.2.1",
-<<<<<<< HEAD
+    "cordova-plugin-whitelist": "1.2.2",
     "org.flybuy.cordova.background-location-services": "1.0.0",
     "org.frd49.cordova.exitapp": "1.0.0",
     "uk.co.workingedge.phonegap.plugin.launchnavigator": "3.0.4"
-=======
-    "cordova-plugin-whitelist": "1.2.2"
->>>>>>> parent of 1b99a16... update changes
 };
 // BOTTOM OF METADATA
 });
