d8a9aa4a6f215be06268177128885c53e136163b
sgastanadui <santiagogerman@hotmail.com>
2016-08-12 14:32:51 -0500

    update

--- plugins/org.flybuy.cordova.background-location-services/README.md
+++ plugins/org.flybuy.cordova.background-location-services/README.md
@@ -2,22 +2,33 @@
 Background Geolocation For Android and iOS with pure javascript callbacks.
 
 #### What is this?
-This plugin is for enabling background geolocation in your cordova project. It was aimed with the specific goal of normalizing the API for android and iOS and retrieving constant location updates in the background until you tell it to stop (If you tell it you want updates every 3 seconds it will give you one every 3 seconds). This is currently in active development. Feel free to make any requests. 
+This plugin is for enabling background geolocation in your cordova project. It was aimed with the specific goal of normalizing the API for android and iOS and retrieving constant location updates in the background until you tell it to stop (If you tell it you want updates every 3 seconds it will give you one every 3 seconds). This is currently in active development. Feel free to make any requests. Below are features I am currently working on.
 
-I've also included an activity detection API. It is used to save battery life, but you can also retrieve the likelihood of what the user is currently doing (standing still, walking, running, driving, etc).
+### Todo: 
+ * Both: 
+  * SQLlite Storage
+  * Enable better distance filtering, possibly by speed readings
+  * Test
+  * Documentation
+  * Demos (Regular Node, Meteor)
+  * ~~Meteor Specific Atmosphere Package~~
+  * Npm
+ * Android:
+  * ~~Integrated Detected Activities for better battery life~~
+  * ~~Make Icon Notification user configureable~~
+  * ~~Get Android callbacks to work, always.~~
+ * iOS
+  * Enable switching to only using significant changes and back 
 
-### Changelog :
- * 1.0.3 Activity Detection And Much Better Battery Life For iOS!
- * 1.0.2 Error callbacks now correctly funnel through the location register
- 
 ### Techniques used:
 
 **Android** : Uses Fused Location API and Activity Recognition API to serve location updates endlessly.
 
-**iOS** : Uses a timer based approach and CoreMotion library to enable endless background tracking.
+**iOS** : Uses a timer based approach to enable endless background tracking.
 
 ###Setup: 
-* Make sure you have Google Play Services AND Google Repository installed via your android-sdk manager prior to building your application with this. It will be under the extras part of the sdk manager. More information can be found here: http://developer.android.com/sdk/installing/adding-packages.html.
+* Need to make sure you have Google Play Services AND Google Repository installed via your android-sdk manager prior to building your application with this. It will be under the extras part of the sdk manager. More information can be found here: http://developer.android.com/sdk/installing/adding-packages.html.
+* 
 
 ###Installation:
 
@@ -41,11 +52,7 @@ window.plugins.backgroundLocationServices
 ````javascript
 
 //Make sure to get at least one GPS coordinate in the foreground before starting background services
-navigator.geolocation.getCurrentPosition(function() {
- console.log("Succesfully retreived our GPS position, we can now start our background tracker.");
-}, function(error) {
- console.error(error);
-});
+navigator.geolocation.getCurrentPosition();
 
 //Get plugin
 var bgLocationServices =  window.plugins.backgroundLocationServices;
@@ -57,12 +64,11 @@ bgLocationServices.configure({
      distanceFilter: 5, // (Meters) How far you must move from the last point to trigger a location update
      debug: true, // <-- Enable to show visual indications when you receive a background location update
      interval: 9000, // (Milliseconds) Requested Interval in between location updates.
-     useActivityDetection: true, // Uses Activitiy detection to shut off gps when you are still (Greatly enhances Battery Life)
-     
      //Android Only
      notificationTitle: 'BG Plugin', // customize the title of the notification
      notificationText: 'Tracking', //customize the text of the notification
-     fastestInterval: 5000 // <-- (Milliseconds) Fastest interval your app / server can handle updates
+     fastestInterval: 5000, // <-- (Milliseconds) Fastest interval your app / server can handle updates
+     useActivityDetection: true // Uses Activitiy detection to shut off gps when you are still (Greatly enhances Battery Life)
      
 });
 
@@ -73,13 +79,11 @@ bgLocationServices.registerForLocationUpdates(function(location) {
      console.log("Error: Didnt get an update", err);
 });
 
-//Register for Activity Updates
-
-//Uses the Detected Activies / CoreMotion API to send back an array of activities and their confidence levels
-//See here for more information:
-//https://developers.google.com/android/reference/com/google/android/gms/location/DetectedActivity
-bgLocationServices.registerForActivityUpdates(function(activities) {
-     console.log("We got an activity update" + activities);
+//Register for Activity Updates (ANDROID ONLY)
+//Uses the Detected Activies API to send back an array of activities and their confidence levels
+//See here for more information: //https://developers.google.com/android/reference/com/google/android/gms/location/DetectedActivity
+bgLocationServices.registerForActivityUpdates(function(acitivites) {
+     console.log("We got an BG Update" + activities);
 }, function(err) {
      console.log("Error: Something went wrong", err);
 });
@@ -91,12 +95,6 @@ bgLocationServices.start();
 ///later, to stop
 bgLocationServices.stop();
 ````
-
-### Known Issues:
-
-Phonegap Build : Swift files are not officially supported as of yet on phonegap build, so if there is a problem installing it in that environment, there isn't anything I can do until they are supported.
-
-### Credit!
 
 By the way, credit to Christocracy and his great [plugin](https://github.com/christocracy/cordova-plugin-background-geolocation/tree/0.3.7) that spurned this one. It should share the same concepts via javascript.
 
--- plugins/org.flybuy.cordova.background-location-services/src/ios/CDVBackgroundLocationServices.swift
+++ plugins/org.flybuy.cordova.background-location-services/src/ios/CDVBackgroundLocationServices.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 import CoreLocation
-import CoreMotion
-
 
 let TAG = "[LocationServices]";
 let PLUGIN_VERSION = "1.0";
@@ -21,7 +19,6 @@ func log(message: String){
 }
 
 var locationManager = LocationManager();
-var activityManager = ActivityManager();
 var taskManager = TaskManager();
 
 //Option Vars
@@ -29,12 +26,9 @@ var distanceFilter = kCLDistanceFilterNone;
 var desiredAccuracy = kCLLocationAccuracyBest;
 var activityType = CLActivityType.AutomotiveNavigation;
 var interval = 5.0;
-var debug: Bool?;
-var useActivityDetection = false;
 var aggressiveInterval = 2.0;
-
-var stationaryTimout = (Double)(5 * 60); // 5 minutes
 var backgroundTaskCount = 0;
+var debug: Bool?;
 
 //State vars
 var enabled = false;
@@ -42,9 +36,6 @@ var background = false;
 
 var locationUpdateCallback:String?;
 var locationCommandDelegate:CDVCommandDelegate?;
-
-var activityUpdateCallback:String?;
-var activityCommandDelegate:CDVCommandDelegate?;
 
 
 @objc(HWPBackgroundLocationServices) class BackgroundLocationServices : CDVPlugin {
@@ -58,19 +49,19 @@ var activityCommandDelegate:CDVCommandDelegate?;
 
         NSNotificationCenter.defaultCenter().addObserver(
             self,
-            selector: #selector(BackgroundLocationServices.onResume),
+            selector: "onResume",
             name: UIApplicationWillEnterForegroundNotification,
             object: nil);
 
         NSNotificationCenter.defaultCenter().addObserver(
             self,
-            selector: #selector(BackgroundLocationServices.onSuspend),
+            selector: "onSuspend",
             name: UIApplicationDidEnterBackgroundNotification,
             object: nil);
 
         NSNotificationCenter.defaultCenter().addObserver(
             self,
-            selector: #selector(BackgroundLocationServices.willResign),
+            selector: "willResign",
             name: UIApplicationWillResignActiveNotification,
             object: nil);
     }
@@ -84,7 +75,6 @@ var activityCommandDelegate:CDVCommandDelegate?;
     // 6 notificationTitle -- (not used on ios),
     // 7 notificationText-- (not used on ios),
     // 8 activityType, fences -- (not used ios)
-    // 9 useActivityDetection
     func configure(command: CDVInvokedUrlCommand) {
 
         //log("configure arguments: \(command.arguments)");
@@ -95,7 +85,6 @@ var activityCommandDelegate:CDVCommandDelegate?;
         aggressiveInterval = (Double)(command.argumentAtIndex(4) as! Int / 1000); // Millseconds to seconds
         activityType = self.toActivityType(command.argumentAtIndex(8) as! String);
         debug = command.argumentAtIndex(5) as? Bool;
-        useActivityDetection = command.argumentAtIndex(9) as! Bool;
 
         log("--------------------------------------------------------");
         log("   Configuration Success");
@@ -116,21 +105,21 @@ var activityCommandDelegate:CDVCommandDelegate?;
     }
 
     func registerForActivityUpdates(command : CDVInvokedUrlCommand) {
-        log("registerForActivityUpdates");
-        activityUpdateCallback = command.callbackId;
-        activityCommandDelegate = commandDelegate;
+        log("Activity Updates not enabled on IOS");
+
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: "Activity Updates not enabled on IOS");
+        commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId);
     }
 
 
     func start(command: CDVInvokedUrlCommand) {
         log("Started");
         enabled = true;
-
+        
         log("Are we in the background? \(background)");
-
+        
         if(background) {
-            locationManager.startUpdating(false);
-            activityManager.startDetection();
+            locationManager.startUpdating();
         }
 
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
@@ -142,7 +131,6 @@ var activityCommandDelegate:CDVCommandDelegate?;
         enabled = false;
 
         locationManager.stopBackgroundTracking();
-        activityManager.stopDetection();
 
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
         commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
@@ -184,7 +172,6 @@ var activityCommandDelegate:CDVCommandDelegate?;
 
         taskManager.endAllBackgroundTasks();
         locationManager.stopUpdating();
-        activityManager.stopDetection();
     }
 
     func onSuspend() {
@@ -192,8 +179,7 @@ var activityCommandDelegate:CDVCommandDelegate?;
         background = true;
 
         if(enabled) {
-            locationManager.startUpdating(false);
-            activityManager.startDetection();
+            locationManager.startUpdating();
         }
     }
 
@@ -202,8 +188,7 @@ var activityCommandDelegate:CDVCommandDelegate?;
         background = true;
 
         if(enabled) {
-            locationManager.startUpdating(false);
-            activityManager.startDetection();
+            locationManager.startUpdating();
         }
     }
 
@@ -253,8 +238,6 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
     var locationArray = [CLLocation]();
     var updating = false;
     var aggressive = false;
-    
-    var lowPowerMode = false;
 
     override init() {
         super.init();
@@ -334,12 +317,7 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
             let longitude = bestLocation!.coordinate.longitude;
             let accuracy = bestLocation!.horizontalAccuracy;
 
-            var msg = "Got Location Update:  { \(latitude) - \(longitude) }  Accuracy: \(accuracy)";
-            
-            if(useActivityDetection) {
-                    msg += " Stationary : \(activityManager.isStationary)";
-            }
-            
+            let msg = "Got Location Update:  { \(latitude) - \(longitude) }  Accuracy: \(accuracy)";
             log(msg);
             NotificationManager.manager.notify(msg);
 
@@ -366,27 +344,20 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
         distanceFilter = 0;
     }
 
-    // Force here is to make sure we are only starting the location updates once, until we want to restart them
-    // Was having issues with it starting, and then starting a second time through resign on some builds.
-    func startUpdating(force : Bool) {
-        if(!self.updating || force) {
-            self.enableBackgroundLocationUpdates();
-            self.updating = true;
+    func startUpdating() {
+        self.enableBackgroundLocationUpdates();
+        self.updating = true;
 
-            self.manager.delegate = self;
+        self.manager.delegate = self;
+        self.manager.desiredAccuracy = desiredAccuracy;
+        self.manager.distanceFilter = distanceFilter;
 
-            self.manager.desiredAccuracy = self.lowPowerMode ? kCLLocationAccuracyThreeKilometers : desiredAccuracy;
-            self.manager.distanceFilter = self.lowPowerMode ? 10.0 : distanceFilter;
+        self.manager.startUpdatingLocation();
+        self.manager.startMonitoringSignificantLocationChanges();
 
-            self.manager.startUpdatingLocation();
-            self.manager.startMonitoringSignificantLocationChanges();
+        taskManager.beginNewBackgroundTask();
 
-            taskManager.beginNewBackgroundTask();
-
-            log("Starting Location Updates!");
-        } else {
-            log("A request was made to start Updating, but the plugin was already updating")
-        }
+        log("Starting Location Updates!");
     }
 
     func stopUpdating() {
@@ -403,6 +374,7 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
     }
 
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        log("Got location update");
         let locationArray = locations as NSArray
         let locationObj = locationArray.lastObject as! CLLocation
 
@@ -426,14 +398,14 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
 
         taskManager.beginNewBackgroundTask();
 
-        locationTimer = NSTimer.scheduledTimerWithTimeInterval(activityManager.isStationary ? stationaryTimout : interval, target: self, selector: #selector(LocationManager.restartUpdates), userInfo: nil, repeats: false);
+        locationTimer = NSTimer.scheduledTimerWithTimeInterval(interval, target: self, selector: Selector("restartUpdates"), userInfo: nil, repeats: false);
 
         if(stopUpdateTimer != nil) {
             stopUpdateTimer.invalidate();
             stopUpdateTimer = nil;
         }
 
-        stopUpdateTimer = NSTimer.scheduledTimerWithTimeInterval(syncSeconds, target: self, selector: #selector(LocationManager.syncAfterXSeconds), userInfo: nil, repeats: false);
+        stopUpdateTimer = NSTimer.scheduledTimerWithTimeInterval(syncSeconds, target: self, selector: Selector("syncAfterXSeconds"), userInfo: nil, repeats: false);
     }
 
     func restartUpdates() {
@@ -442,24 +414,15 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
             locationTimer.invalidate();
             locationTimer = nil;
         }
-        
-        self.lowPowerMode = false;
 
         self.manager.delegate = self;
         self.manager.desiredAccuracy = desiredAccuracy;
         self.manager.distanceFilter = distanceFilter;
 
-        self.startUpdating(true);
-    }
-    
-    func setGPSLowPower() {
-        log("Setting GPS To Low Power Mode ");
-        self.lowPowerMode = true;
-        self.startUpdating(true);
+        self.startUpdating();
     }
 
     func syncAfterXSeconds() {
-        self.setGPSLowPower();
         self.sync();
         log("Stopped Location Updates After \(syncSeconds)");
     }
@@ -474,16 +437,6 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
 
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
         log("LOCATION ERROR: \(error.description)");
-
-        locationCommandDelegate?.runInBackground({
-
-            var result:CDVPluginResult?;
-
-            result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: error.description);
-            result!.setKeepCallbackAsBool(true);
-            locationCommandDelegate?.sendPluginResult(result, callbackId:locationUpdateCallback);
-        });
-
 
     }
     func locationManager(manager: CLLocationManager, didFinishDeferredUpdatesWithError error: NSError?) {
@@ -501,127 +454,6 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
         }
     }
 
-}
-
-class ActivityManager : NSObject {
-    var manager : CMMotionActivityManager?;
-    var available = false;
-    var updating = false;
-    
-    var isStationary = false;
-    
-    override init() {
-        super.init();
-        
-        if(CMMotionActivityManager.isActivityAvailable()) {
-            log("Activity Manager is Available");
-            
-            self.manager = CMMotionActivityManager();
-            self.available = true;
-        } else {
-            log("Activity Manager is not Available");
-        }
-    }
-    
-    func confidenceToInt(confidence : CMMotionActivityConfidence) -> Int {
-        var confidenceMult = 0;
-        
-        switch(confidence) {
-            case CMMotionActivityConfidence.High :
-                confidenceMult = 100;
-                break;
-            case CMMotionActivityConfidence.Medium :
-                confidenceMult = 50;
-                break;
-            case CMMotionActivityConfidence.Low :
-                confidenceMult = 0;
-                break;
-            default:
-                confidenceMult = 0;
-                break;
-        }
-        
-        return confidenceMult;
-    }
-    
-    func getActivityConfidence(detectedActivity : Bool, multiplier : Int) -> Int {
-        return (detectedActivity ? 1 : 0) * multiplier;
-    }
-    
-    func activitiesToArray(data:CMMotionActivity) -> NSDictionary {
-        let confidenceMult = self.confidenceToInt(data.confidence);
-        
-        var detectedActivities:Dictionary = [
-            "UNKOWN" : self.getActivityConfidence(data.unknown, multiplier: confidenceMult),
-            "STILL" : self.getActivityConfidence(data.stationary, multiplier: confidenceMult),
-            "WALKING" : self.getActivityConfidence(data.walking, multiplier: confidenceMult),
-            "RUNNING" : self.getActivityConfidence(data.running, multiplier: confidenceMult),
-            "IN_VEHICLE" : self.getActivityConfidence(data.automotive, multiplier: confidenceMult)
-        ];
-        
-        // Cycling Only available on IOS 8.0
-        if #available(iOS 8.0, *) {
-            detectedActivities["ON_BICYCLE"] = self.getActivityConfidence(data.cycling, multiplier: confidenceMult)
-        }
-        
-        log("Received Detected Activities : \(detectedActivities)");
-        
-        return detectedActivities;
-    }
-
-    func sendActivitiesToCallback(activities : NSDictionary) {
-        if(activityCommandDelegate != nil) {
-            activityCommandDelegate?.runInBackground({
-                
-                var result:CDVPluginResult?;
-                
-                result = CDVPluginResult(status: CDVCommandStatus_OK, messageAsDictionary:activities as [NSObject: AnyObject]);
-                result!.setKeepCallbackAsBool(true);
-                activityCommandDelegate?.sendPluginResult(result, callbackId:activityUpdateCallback);
-            });
-        }
-    }
-    
-    func startDetection() {
-        NSLog("Activity Manager - Starting Detection %@", self.available);
-        if(useActivityDetection == false) {
-            return;
-        }
-        
-        if(self.available) {
-            self.updating = true;
-            
-            manager!.startActivityUpdatesToQueue(NSOperationQueue()) { data in
-                if let data = data {
-                    dispatch_async(dispatch_get_main_queue(), {
-                        if(data.stationary == true) {
-                            self.isStationary = true;
-                        } else {
-                            if(self.isStationary == true) {
-                                locationManager.restartUpdates();
-                            }
-                            
-                            self.isStationary = false
-                            
-                        }
-                        
-                        self.sendActivitiesToCallback(self.activitiesToArray(data));
-                        
-                    })
-                }
-            }
-        } else {
-            log("Activity Manager - Not available on your device");
-        }
-    }
-    
-    func stopDetection() {
-        if(self.available && self.updating) {
-            self.updating = false;
-            
-            manager!.stopActivityUpdates();
-        }
-    }
 }
 
 var backgroundTimer: NSTimer!
@@ -675,7 +507,7 @@ class TaskManager : NSObject {
         if(app.respondsToSelector("endBackgroundTask")) {
             let count = self._bgTaskList.count;
 
-            for _ in 0 ..< count {
+            for var i = 0; i < count; i++ {
                 let bgTaskId = self._bgTaskList[0] as Int;
                 log("Ending Background Task  with ID \(bgTaskId)");
                 app.endBackgroundTask(bgTaskId);
